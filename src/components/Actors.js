999d79f5f1338a55043f6a0bffc31450c43dcf74
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-11-15 20:28:08 +0300

    updated

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actors.map((actor,index)=>(
+      <div key={index}>
+        <h2>name:{actor.name}</h2>
+        <ul>
+          {actors.map((movies,moviesIndex)=>(
+            <li key={moviesIndex}>{movies}</li>
+          ))}
+        </ul>
+        </div>
+        ))}
+  </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    
+      {movies.map((movies,index)=>(
+        <div key={index}>
+          <h2>Name{movies.title}</h2>
+          <p>Time:{movies.time}</p>
+          <ul>
+            {movies.genres.map((genre,genreIndex)=>(
+              <li key={genreIndex}>{genre}</li>
+            ))}
+          </ul>
+          </div>
+      ))}
+    
+    </div>;
 }
 
 export default Movies;
